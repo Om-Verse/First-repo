3c0ea6e9f49fcd33ea302e812d38841f975b5340
Om-Verse <ompatre.dev@gmail.com>
2026-08-29 23:55:08 +0530

    add libary management sys to main.py

---
+++ Maker_day/LIb_Mgmt_sys/main.py
@@ -0,0 +1,18 @@
+import colorama
+from colorama import Fore, Back, Style, init
+
+def menu():
+    init(autoreset=True)
+    print(Fore.RED + '''╔══════════════════════════════════════════════╗
+║                                              ║
+║        📚  LIBRARY MANAGEMENT SYSTEM  📚     ║
+║                                              ║
+╚══════════════════════════════════════════════╝''')
+    
+
+# Initialize colorama (autoreset=True reverts to default color after every print)
+
+
+
+
+menu()
